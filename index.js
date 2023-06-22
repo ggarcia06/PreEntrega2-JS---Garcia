3a3be4042771b0e7d23cfcacb4a01de5766caa74
German Garcia <ggarcia@g2tecnologia.com.ar>
2023-06-21 21:01:38 -0300

    Segunda pre-entrega

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 
-
+// funciones
 const calcularImpuestos = (total) => {return total * 1.75}
-
 
 const precioConDescuento = (total, descuento) => {return total * descuento};
 
+function Persona(nombre, apellido, email) {
+    this.nombre = prompt("Ingrese su nombre");
+    this.apellido = prompt("Ingrese su apellido");
+    this.email = prompt("Ingrese su e-mail");
+} 
+
+//variables
+let nombre = "nombre"
+let apellido = "apellido";
+let email = "email";
+
+// declaracion de un array para obtener datos de los usuarios y ejecucion del simulador 
+const usuarios = [];
+for(let i = 0; i < 3; i++){
+    usuarios.push(new Persona(i));
 
 
 alert("Bienvenido, el siguiente simulador le ayudara a cotizar sus proximas vacasiones");
 
+
 let numeroDeNoches = parseInt(prompt("Ingrese el numero de noches. (minimo 4, maximo 10)"));
+usuarios[i].numeroDeNoches = numeroDeNoches;
 let totalSinImpuestos = 0;
 let resultado = 0
-
 
 
 if (numeroDeNoches >= 4 && numeroDeNoches <= 10) {
     let opcion = parseInt(prompt("Seleccione una opcion para el destino deseado: 1- Miami, USA / 2- Cancun, Mexico / 3- La Habana, Cuba / 4- Lima, Peru / 5- SALIR "));
     const cerrarAplicacion = 5;
+    usuarios[i].opcion = opcion;
 
     while (opcion != 5) {
 
@@ -26,6 +42,7 @@ if (numeroDeNoches >= 4 && numeroDeNoches <= 10) {
             case 1:
                 totalSinImpuestos = 132000 * numeroDeNoches;
                 resultado = calcularImpuestos(totalSinImpuestos);
+                usuarios[i].resultado = resultado;
                 alert("El valor del paquete con impuestos incluidos es de $" + resultado + " ARG.");
                 let codigoDescuento = prompt("Si tiene codigo de descuento ingreselo, ESC para salir")
                 if(codigoDescuento == "MIAMI365"){
@@ -40,11 +57,13 @@ if (numeroDeNoches >= 4 && numeroDeNoches <= 10) {
             case 2:
                 totalSinImpuestos = 106500 * numeroDeNoches;
                 resultado = calcularImpuestos(totalSinImpuestos);
+                usuarios[i].resultado = resultado;
                 alert("El valor del paquete con impuestos incluidos es de $" + resultado + " ARG.");
                 break;
             case 3:
                 totalSinImpuestos = 124000 * numeroDeNoches;
                 resultado = calcularImpuestos(totalSinImpuestos);
+                usuarios[i].resultado = resultado;
                 alert("El valor del paquete con impuestos incluidos es de $" + resultado + " ARG.");
                 let codigoDescuento3 = prompt("Si tiene codigo de descuento ingreselo, ESC para salir")
                 if(codigoDescuento3 == "HABANA365"){
@@ -58,15 +77,31 @@ if (numeroDeNoches >= 4 && numeroDeNoches <= 10) {
             case 4:
                 totalSinImpuestos = 102300 * numeroDeNoches;
                 resultado = calcularImpuestos(totalSinImpuestos);
+                usuarios[i].resultado = resultado;
                 alert("El valor del paquete con impuestos incluidos es de $" + resultado + " ARG.");
                 break;
             default:
                 alert("ingreso mal la opcion");
                 break;
         }
-        alert("Gracias por su consulta, regrese pronto");
+        alert("Gracias por su consulta " + usuarios[i].nombre + ". Estaremos enviando un correo con el valor del destino seleccionado al correo: " + usuarios[i].email + " .Regrese pronto");
         opcion = prompt("escriba 5 para salir");
     }
 } else {
     alert("El número ingresado no es valido, los paquetes de viaje se arman a partir de 4 noches hasta 10 noches inclusive, ingrese nuevamente al simulador")
 }
+
+}
+console.log(usuarios);
+
+
+//filtrado de la lista de usuarios por seleccion de destino
+const usuariosDestinoMiami = usuarios.filter((el) => el.opcion == 1);
+const usuariosDestinoCancun = usuarios.filter((el) => el.opcion == 2);
+const usuariosDestinoHabana = usuarios.filter((el) => el.opcion == 3);
+const usuariosDestinoLima = usuarios.filter((el) => el.opcion == 4);
+
+console.log(usuariosDestinoMiami);
+console.log(usuariosDestinoCancun);
+console.log(usuariosDestinoHabana);
+console.log(usuariosDestinoLima);
